87caeca098ed9f8c300419492ee52498e614f53d
kkh <gogogid7@gmail.com>
2021-05-25 16:49:42 +0900

    새로 업로드02
    test

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct BaseStats {
+	float hp;
+	float damage;
+	float attackSpeed;
+	float moveSpeed;
+}
+
+public class Stats {
+	float hp;
+	float damage;
+	float attackSpeed;
+	float moveSpeed;
+	
+
+	public Stats() {
+		
+	}	
+}
