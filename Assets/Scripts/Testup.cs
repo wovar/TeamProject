14d59bbf431b31af5aa8f38b6a74d61cb1477710
kkh <gogogid7@gmail.com>
2021-05-25 17:22:27 +0900

    test001

--- Assets/Scripts/Testup.cs
+++ Assets/Scripts/Testup.cs
@@ -7,12 +7,12 @@ public class Testup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //test
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //test
+        
     }
 }
