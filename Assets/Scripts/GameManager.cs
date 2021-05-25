b3d5dbfe1635e7e35486b2784d8b39a878752132
kkh <gogogid7@gmail.com>
2021-05-25 16:49:42 +0900

    Test04
    test4

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private static GameManager m_instance;
+	public static GameManager Instance {
+		get {
+			if (!m_instance)
+				m_instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+			return m_instance;
+		}
+	}
+
+	private Action m_inIt;
+
+	/// <summary>
+	/// 매니저 클래스 초기화 작업
+	/// </summary>
+	/// <param name="method"></param>
+	public void Init (Action method) {	m_inIt += method; }
+
+	private void Awake () {
+		if (m_instance == null) {
+			m_instance = this;
+		} else if (m_instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+		
+		if(m_inIt != null)
+			m_inIt();
+	}
+
+	private void Start () {
+		
+	}
+
+	private void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct BaseStats {
+	float hp;
+	float damage;
+	float attackSpeed;
+	float moveSpeed;
+}
+
+public class Stats {
+	float hp;
+	float damage;
+	float attackSpeed;
+	float moveSpeed;
+	
+
+	public Stats() {
+		//Test test test
+		int a = 0;
+	}	
+}
